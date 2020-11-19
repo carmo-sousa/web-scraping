490f0e93013317bd8145d5c8551e546e0787914e
Rômulo do Carmo Sousa <carmosousa20@outlook.com>
2020-11-19 15:55:10 -0400

    Refatorando a funçao de email

--- src/email.py
+++ src/email.py
@@ -2,29 +2,22 @@
 Envia um e-mail com uma lista dos melhores filmes de todos os tempos da url
 http://www.adorocinema.com/filmes/todos-filmes/notas-espectadores/
 """
-
-import os
 import smtplib
 
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 
-def mail(message):
-    HOST = "smtp.gmail.com"
-    PORT = 587
-    USER = os.getenv("MAIL_FROM")
-    PASSWORD = os.getenv("PASSWORD")
-    TO = os.getenv("MAIL_TO")
+def mail(message, host, port, user, password, mail_to):
 
-    server = smtplib.SMTP(HOST, PORT)
+    server = smtplib.SMTP(host, port)
     server.ehlo()
     server.starttls()
-    server.login(USER, PASSWORD)
+    server.login(user, password)
 
     email_msg = MIMEMultipart()
-    email_msg["From"] = USER
-    email_msg["To"] = TO
+    email_msg["From"] = user
+    email_msg["To"] = mail_to
     email_msg["Subject"] = "teste de envio de email"
     email_msg.attach(MIMEText(message, "html"))
 
