26b791139832f9599e2f3e22f3da3269ea101dab
Rômulo do Carmo Sousa <carmosousa20@outlook.com>
2020-11-19 15:54:45 -0400

    Refatorando a funçao app

--- app.py
+++ app.py
@@ -16,6 +16,11 @@ load_dotenv()
 
 
 def app():
+    HOST = os.getenv("HOST")
+    PORT = os.getenv("PORT")
+    USER = os.getenv("MAIL_FROM")
+    PASSWORD = os.getenv("PASSWORD")
+    TO = os.getenv("MAIL_TO")
     URL = os.getenv("URL")
 
     print("⚙ Pegando os filmes...")
@@ -25,7 +30,7 @@ def app():
     mount_html = html(get_movies)
 
     print("⚙ Enviando o e-mail...")
-    mail(mount_html)
+    mail(mount_html,HOST, PORT, USER, PASSWORD, TO)
     print("✔ E-mail enviado!")
 
 
